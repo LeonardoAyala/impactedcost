3a0acf19f86640e41bd36c3c6ee87db1a87c7419
Leonardo Ayala <ayala.santiago.leonardo@gmail.com>
2019-06-05 11:53:01 -0500

    Jquery widgets added to Laravel Mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import $ from 'jquery';
+window.$ = window.jQuery = $;
 
 /**
  * The following block of code may be used to automatically register your
